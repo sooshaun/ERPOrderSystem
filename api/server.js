7b1f5458a2f2cefb8b80aa27d47bc7ae869a8dae
Shaun <soo.shaun@gmail.com>
2018-01-10 10:35:44 +1100

    Add cors

--- api/server.js
+++ api/server.js
@@ -5,14 +5,15 @@ if (process.env.NODE_ENV !== 'production') {
 const port = 7000
 const express = require('express')
 const bodyParse = require('body-parser')
+const cors = require('cors')
 const authMiddleware = require('./middleware/auth')
-
 
 
 const server = express()
 
 server.use([
   bodyParse.json(),
+  cors(),
   authMiddleware.initialize,
 ]
 )
@@ -24,7 +25,6 @@ server.get('/', function (req, res) { res.send('Hello World....') })
 // Error handler
 server.use(function (req, res, next) { res.status(404); res.send("404 - not found"); })
 
-
 // Error handler
 server.use((error, req, res, next) => {
   res.json({
@@ -33,7 +33,6 @@ server.use((error, req, res, next) => {
     }
   })
 })
-
 
 // To test whether the server is reponding to the terminal
 server.listen(port, (error) => {
